3b54b32c8974b7098945f55b79eb6bcb1c193d03
rabibasukala01 <63946883+rabibasukala01@users.noreply.github.com>
2022-08-09 20:43:47 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,173 @@
+// alert('chello world');
+// confirm('dwad')
+
+// console.log("hello world")
+// console.warn('warning message')
+// console.error("error ")
+// console.clear()
+// console.assert()
+
+
+// VARIABLES:
+// var number = 1;
+// let number=1; block level scope
+// const num=1; constant
+
+
+// DATATYPE:
+// var name = 'sam k';
+// var num = 102;
+
+// var dict = {
+//     nepal: 'kath',
+//     india: 'new'
+// };
+
+// var conditions = true;
+
+// var defines = undefined; //or var defines
+
+// var a = null;
+
+// var arr = [1, 2, 3, 4];
+// var arr = [1, 'dwadad', 3, 4];
+
+
+
+// FUNCTIONS:
+
+// function add(a, b) {
+//     c = a + b
+//     return c
+// }
+
+// add = (a, b) => {
+//     console.log(a + b)
+// }
+
+// add(10, 20)
+
+
+// CONDITIONS:
+// var num = 100;
+
+// if (num != 100) {
+//     console.log("Not century")
+// }
+// else {
+//     console.log('century')
+// }
+
+// else if()
+
+
+// LOOPS:
+
+// for (var i = 0; i <= 4; i++) {
+//     console.log(i)
+
+// }
+
+// var arr = ['rr', 22, 83, 84, 905, 06]
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function (element) {
+//     console.log(element);
+// })
+
+// let n = 10;
+// while (n != 0) {
+//     console.log(n)
+//     n--;
+// }
+
+
+// break;
+// continue;
+
+
+// STRING SLICE
+
+// let mystr = "hello world";
+// console.log(mystr.slice(1, 5))
+// similar to python
+
+
+// let mydate = new Date()
+// console.log(mydate)
+
+
+
+
+
+// DOM MANUPULATIONS
+// document.write('writting')
+
+
+
+// document.getElementById('idname');
+// classelement = document.getElementsByClassName('classname');//provide arr
+// document.getElementsByTagName('tagname');//provide arr
+
+
+// classelement[0].style.backgroundColor = 'white';
+// classelement[0].classList.add('css class')
+// classelement[0].innerHTML = 'wow'
+// console.log(classelement[0].innerHTML)
+
+// a=document.createElement('a');
+// appendChild(a)
+// replaceChild(b,a)
+// removeChild(element)
+
+
+
+// QUERY SELECTOR
+// document.querySelector('.classname')// return first element
+// document.querySelectorAll('.classname')//return all element
+// a = document.querySelector('#idname');
+// a.style.fontSize = "99px";
+
+
+// Events
+
+// idname.addEventListener('click', function () {
+//     console.log('addEventListener')
+// })
+
+// 'mousehover'
+// 'mouseout'
+// 'mouseup'
+// 'mousedown'
+// etc
+
+
+// SETTIMEOUT & SETINTERVAL
+// setTimeout(functionName, 200); //ms
+// setInterval(functionName, 500);//ms
+
+
+// LOCAL-STORAGE
+// store key-value pair locally
+// localStorage.setItem('name', 'sam');
+// localStorage.getItem('name')
+// localStorage.clear()
+// etc
+
+
+
+// JSON-JAVASCRIPT OBJECT NOTATION
+// obj = {
+//     name: "wow",
+//     age: 22
+// }
+// j = JSON.stringify(obj) //object to string
+
+// resultofj = {
+//     "name": "wow",
+//     "age": 22
+// }
+
+// obj = JSON.parse(j) // string to object
